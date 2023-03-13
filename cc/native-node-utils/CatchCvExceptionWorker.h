116bfbacdcf38bac52dce596499d5ff6ec808852
UrielCh <uriel.chemouni@gmail.com>
2023-03-13 09:43:14 +0200

    C++: add comment / defrag code

--- cc/native-node-utils/CatchCvExceptionWorker.h
+++ cc/native-node-utils/CatchCvExceptionWorker.h
@@ -3,9 +3,12 @@
 #ifndef __FF_CATCHCVEXCEPTIONWORKER_H__
 #define __FF_CATCHCVEXCEPTIONWORKER_H__
 
-// TODO remove this one
 namespace FF {
 
+	/**
+	 * @brief Base class implementing default ISyncWorker and IAsyncWorker
+	 * 
+	 */
 	class SimpleWorkerBase : public ISyncWorker, public IAsyncWorker {
 	public:
 		virtual bool unwrapRequiredArgs(Nan::NAN_METHOD_ARGS_TYPE info) = 0;
@@ -13,18 +16,41 @@ namespace FF {
 		virtual bool unwrapOptionalArgsFromOpts(Nan::NAN_METHOD_ARGS_TYPE info) = 0;
 		virtual bool hasOptArgsObject(Nan::NAN_METHOD_ARGS_TYPE info) = 0;
 
+		/**
+		 * @brief Return empty string by default at execution time fullfill the IWorker interface
+		 * 
+		 * @return std::string 
+		 */
 		std::string execute() {
 			return "";
 		}
 
+		/**
+		 * @brief Return undefined by default for SyncWorker
+		 * 
+		 * @return v8::Local<v8::Value> 
+		 */
 		v8::Local<v8::Value> getReturnValue() {
 			return Nan::Undefined();
 		}
 
+		/**
+		 * @brief Return same result as SyncWorker by default for AsyncWorker
+		 * 
+		 * @param info 
+		 * @return v8::Local<v8::Value> 
+		 */
 		v8::Local<v8::Value> getReturnValue(Nan::NAN_METHOD_ARGS_TYPE info) {
 			return getReturnValue();
 		}
 
+		/**
+		 * @brief unwrap all required and optional arguments
+		 * 
+		 * @param info 
+		 * @return true 
+		 * @return false 
+		 */
 		bool applyUnwrappers(Nan::NAN_METHOD_ARGS_TYPE info) {
 			return unwrapRequiredArgs(info)
 				|| (!hasOptArgsObject(info) && unwrapOptionalArgs(info))
@@ -32,6 +58,10 @@ namespace FF {
 		}
 	};
 
+	/**
+	 * @brief basic non abstract class implementing default ISyncWorker and IAsyncWorker
+	 * 
+	 */
 	class SimpleWorker : public SimpleWorkerBase {
 		bool unwrapOptionalArgs(Nan::NAN_METHOD_ARGS_TYPE info) {
 			return false;
@@ -52,16 +82,29 @@ namespace FF {
 
 }
 
+/**
+ * @brief Same as SimpleWorker but catch cv::Exception and return error message
+ * add virtual executeCatchCvExceptionWorker() method to implement
+ */
 struct CatchCvExceptionWorker : public FF::SimpleWorker {
 public:
-	std::string execute() {
+    /**
+     * @brief avoid re-declaration of the execute() method, the executeCatchCvExceptionWorker() method must be implemented instead
+     * 
+     * @return std::string 
+     */
+	std::string execute() final {
 		try {
 			return executeCatchCvExceptionWorker();
 		} catch (std::exception &e) {
 			return std::string(e.what());
 		}
 	}
-
+	/**
+	 * @brief implement this method instead of execute() to catch cv::Exception
+	 * 
+	 * @return std::string
+	 */
 	virtual std::string executeCatchCvExceptionWorker() = 0;
 };
 
--- cc/native-node-utils/IWorker.h
+++ cc/native-node-utils/IWorker.h
@@ -6,17 +6,26 @@
 
 namespace FF {
 
+	/**
+	 * @brief Base class for all workers
+	 */
 	class IWorker {
 	public:
 		virtual std::string execute() = 0;
 		virtual bool applyUnwrappers(Nan::NAN_METHOD_ARGS_TYPE info) = 0;
 	};
 
+	/**
+	 * @brief Base class for all sync workers
+	 */
 	class ISyncWorker : public IWorker {
 	public:
 		virtual v8::Local<v8::Value> getReturnValue(Nan::NAN_METHOD_ARGS_TYPE info) = 0;
 	};
 
+	/**
+	 * @brief Base class for all async workers
+	 */
 	class IAsyncWorker : public IWorker {
 	public:
 		virtual v8::Local<v8::Value> getReturnValue() = 0;
